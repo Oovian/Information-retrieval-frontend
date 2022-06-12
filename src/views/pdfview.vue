9a8728c2707291cb9fd6b69009c8851a0a6817d4
oovian <hyeparadox@gmail.com>
2022-06-12 18:34:47 +0800

    pdf

--- src/views/pdfview.vue
+++ src/views/pdfview.vue
@@ -4,28 +4,44 @@ import { VuePdf, createLoadingTask } from 'vue3-pdfjs/esm';
 import { VuePdfPropsType } from 'vue3-pdfjs/components/vue-pdf/vue-pdf-props'; // Prop type definitions can also be imported
 import { PDFDocumentProxy } from 'pdfjs-dist/types/display/api';
 import { watch } from 'vue'
-import axios from 'axios'
-
-
+import { useRouter } from "vue-router";
 export default defineComponent({
   name: 'Home',
   components: { VuePdf },
   setup() {
-    const pdfSrc = ref<VuePdfPropsType['src']>('https://cors-anywhere.herokuapp.com/http://arxiv.org/pdf/1311.2376v3')
-    const numOfPages = ref(0)
-    onMounted(() => {
-      const loadingTask = createLoadingTask(pdfSrc.value)
+    let numOfPages = ref(0)
+    const router = useRouter();
+    const pdfurl = ref(router.currentRoute.value.query.pdfurl);
+    let srcstr = 'https://cors-anywhere.herokuapp.com/'.concat(pdfurl.value as string)
+    let pdfSrc = ref<VuePdfPropsType['src']>(srcstr)
+    
+      let loadingTask = createLoadingTask(pdfSrc.value)
+      console.log(loadingTask)
       loadingTask.promise.then((pdf: PDFDocumentProxy) => {
         numOfPages.value = pdf.numPages
       })
-    })
-    
-
+    watch(
+      // 监听非响应式数据
+      () => router.currentRoute.value,
+      (val) => {
+        // 修改name
+        pdfurl.value = val.query.pdfurl;
+        srcstr='https://cors-anywhere.herokuapp.com/'.concat(pdfurl.value as string)
+        console.log(srcstr)
+        pdfSrc = ref<VuePdfPropsType['src']>(srcstr)
+        loadingTask.destroy()
+        loadingTask = createLoadingTask(pdfSrc.value)
+        console.log(loadingTask)
+        loadingTask.promise.then((pdf: PDFDocumentProxy) => {
+            numOfPages.value = pdf.numPages
+        })
+        });
     return {
       pdfSrc,
       numOfPages
     }
   }
+  
 });
 </script>
 
